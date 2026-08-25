f75b0f2ae7a9f4897d4d70ce3914b1542a84df6d
Lucien-DM <lucien@nuggetcomputersinc.net>
2026-08-25 18:21:33 +1000

    Added validation for the input charset

--- crypt.cpp
+++ crypt.cpp
@@ -3,14 +3,31 @@
 #include <string>
 #include <unistd.h>
 #include <vector>
+#include <set>
 std::string infile = "";
 std::string outfile = "";
-
+const std::string BASE_CHARSET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 inline bool file_exists (const std::string& name) {
 	//This function was taken from stack overflow
 	//checks existence of a file in the FS
 	std::ifstream f(name.c_str());
 	return f.good();
+}
+
+bool validate_charset(std::string charset) {
+	// This verifies that the charset is valid
+	if (charset.length() != 62) {
+		return false;
+	}
+	std::set<char> seen = {};
+	for (char c : charset) {
+		if (seen.find(c) != seen.end()) {
+			return false;
+		} else {
+			seen.insert(c);
+		}
+	}
+	return true;
 }
 
 std::vector<std::string> read_file(std::string fname) {
