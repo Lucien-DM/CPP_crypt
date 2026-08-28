cb69c325f1c97080dd08582f5de8a79731943e03
Lucien-DM <lucien@nuggetcomputersinc.net>
2026-08-28 20:30:04 +1000

    Reverted Previous Commit

--- crypt.cpp
+++ crypt.cpp
@@ -5,14 +5,89 @@
 //TODO: Split this file into multiple source files
 //TODO: Patch out the global comments
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <unistd.h>
 #include <vector>
+#include <set>
+#include <sstream>
 #include <tuple>
-#include "utils.hpp"
+std::string infile = ""; //Input File
+std::string outfile = ""; //Output File
 std::string charset; //Charset for replacement
 int key_i; //Key Itorator
 unsigned int iterate; //iteration value of the charset
-const std::string BASE_CHARSET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"; //This must never change as it will break decryption from earlier versions	
+const std::string BASE_CHARSET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"; //This must never change as it will break decryption from earlier versions
+inline bool file_exists (const std::string& name) {
+	/* This function was taken from stack overflow
+	 * checks existence of a file in the FS */
+	std::ifstream f(name.c_str());
+	return f.good();
+}
+
+inline std::vector<std::string> split (const std::string split_string, char seporator) {
+	/* Code taken from stack overflow
+	 * Splits a string into a vector of strings by a seporator char */
+	std::string segment;
+	std::vector<std::string> seglist;
+	std::stringstream ss(split_string);
+	while(std::getline(ss, segment, seporator))
+	{
+	   seglist.push_back(segment);
+	}
+	return seglist;
+}
+
+bool validate_charset(std::string charset) {
+	/* This verifies that the charset is valid */
+	if (charset.length() != 62) {
+		return false;
+	}
+	if (charset == BASE_CHARSET) {
+		return false;
+	}
+	std::set<char> seen = {};
+	for (char c : charset) {
+		if (seen.find(c) != seen.end()) {
+			return false;
+		} else {
+			seen.insert(c);
+		}
+	}
+	return true;
+}
+
+std::tuple<std::string, int> input_key() {
+	/* Input the key from the user,
+	 * Validate it, and then return as an std::tuple
+	 * if not valid, repeat input until it is
+	 * store charset as std::string charset
+	 * store key itrator as int key_i */
+	std::string unvalidated_charset; //Charset for encryption
+	std::string str_unvalidated_key_i; //Key Itorater Value
+	int key_i;
+	while (true) {
+		std::string key_cin;
+		std::cout << "Key: " << std::flush;
+		std::cin >> key_cin;
+		{
+			std::vector<std::string> temp = split(key_cin, ':');
+			unvalidated_charset = temp[0];
+			str_unvalidated_key_i = temp[1];
+		}
+		if (validate_charset(unvalidated_charset)) {
+			try {
+				key_i = std::stoi(str_unvalidated_key_i);
+				break;
+			} catch (const std::out_of_range& e) {
+				std::cout << "Itorator too big!\nPlease use a smaller one.\n";
+			}
+		} else {
+			std::cout << "Key is Invalid!\n";
+		}		
+	}
+	return std::make_tuple(unvalidated_charset, key_i);
+}	
 
 unsigned int char_counter(const std::vector<std::string>& file, int key_i) {
 	/* recreates the final itorator */
@@ -94,4 +169,36 @@ std::vector<std::string> file_decrypt(std::vector<std::string>& encrypted_file) 
 		line.clear(); //Destructive! Clears line from the vector to free memory
 	}
 	return plain_file;
+}
+
+std::vector<std::string> read_file(std::string fname) {
+	/* Reads file and returns a vector of strings
+	 * Does not validate if file exists */
+	std::vector<std::string> file_contents = {};
+	std::ifstream InputFile(fname); //Opens file - Remember to CLOSE
+	std::string line = ""; //declare and Init line var
+	while (getline (InputFile, line)) {
+		file_contents.push_back(line);
+	}
+	InputFile.close(); //Closes file
+	return file_contents;
+}
+
+int main(int argc, char *argv[]){
+	// first check existance of args
+	if (argc < 4) {
+		std::cout << "E: Too Few Arguments!\n";
+		return 1;
+	}
+	infile = argv[1]; //First argument is input file
+	//Does infile Exist in the filesystem?
+	if (file_exists(infile) == false) {
+		std::cout << '"' << infile << '"' << " Does not exist!\n";
+	}
+	outfile = argv[2]; //Second arg is output file
+	//TODO: Check mode
+	//TODO: Get key
+	//TODO: Read file into memory
+	//TODO: Decrypt file
+	//TODO: Write the file out again.
 }
